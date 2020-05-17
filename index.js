6d5cae7807b27f888a5bca20b038365af17de440
KyleWang <kpeng891011@sina.com>
2020-05-17 21:41:20 +0800

    feat: 添加一些element-ui的常用方法

---
+++ index.js
@@ -0,0 +1,66 @@
+import Vue from 'vue'
+
+const vm = new Vue()
+
+export const showWarning = (message = '') => {
+  vm.$message({
+    type: 'warning',
+    message
+  })
+}
+export const showSuccess = (message = '') => {
+  vm.$message({
+    type: 'success',
+    message
+  })
+}
+export const showError = (message = '') => {
+  vm.$message({
+    type: 'error',
+    message
+  })
+}
+export const showInfo = (message = '') => {
+  vm.$message(message)
+}
+
+export const showConfirm = (str = '', title = '确定删除？', type = 'warning') =>
+  vm.$confirm(str, title, {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type
+  })
+
+/**
+ * el-form属性rules配置
+ * @param {*} message 报错信息
+ * @param {*} validator 追加规则
+ * @returns :void
+ * @example [validator(' 请输入姓名 ')]
+ * @example [validator(' 请输入姓名 '),()=>{}]
+ */
+export const validator = (message = '', validator) => {
+  const obj = { required: true, trigger: 'blur' }
+  if (validator instanceof Function) {
+    return { ...obj, validator }
+  } else {
+    return { ...obj, message }
+  }
+}
+
+/**
+ * 检测el-form
+ * @param {*} ref el-form上挂载的ref引用属性名称
+ * @returns Boolean
+ */
+export const checkForm = ref => {
+  let isPass = false
+  ref.validate(valid => {
+    if (!valid) {
+      return false
+    } else {
+      isPass = valid
+    }
+  })
+  return isPass
+}
