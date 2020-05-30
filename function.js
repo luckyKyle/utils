c4fdcd4df2cb554164865d170534a0d2ba69461b
KyleWang <kpeng891011@sina.com>
2020-05-31 00:02:04 +0800

    feat: 新增可拦截的promise和计算执行消耗时间的两个方法

--- function.js
+++ function.js
@@ -20,3 +20,51 @@ export const copyToClipboard = (str = '') => {
     document.getSelection().addRange(selected)
   }
 }
+
+/**
+ * 为promise对象新增个可拦截的方法
+ * @export
+ * @param {*} promise 传递回来的promise对象
+ * @returns
+ * @example obj = canAbortPromise(promise)   obj.abort("abort this promise")
+ */
+
+export function canAbortPromise(promise) {
+  let res = null
+  let abort = null
+
+  const p1 = new Promise((resolve, reject) => {
+    res = resolve
+    abort = reject
+  })
+
+  p1.abort = abort
+  promise.then(res, abort)
+
+  return p1
+}
+
+export function canAbortPromise2(promise) {
+  const obj = {}
+  const p1 = new Promise((resolve, reject) => {
+    obj.resolve = resolve
+    obj.reject = reject
+  })
+  obj.promise = Promise.race([p1, promise])
+  return obj
+}
+
+/**
+ * 计算一个函数的执行消耗时间
+ * @export
+ * @param {Funcion} fn
+ * @returns 返回时间消耗(毫秒)
+ * @example executeTime(foo) -> 152.0700000000943
+ */
+export function executeConsumeTime(fn) {
+  if (typeof fn !== 'function') throw new Error('fn is not a function')
+  const start = performance.now()
+  fn()
+  const end = performance.now()
+  return end - start
+}
